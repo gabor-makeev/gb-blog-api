8fa30b3605058feb0d0d3dbc21792a47eb2cf8ca
Gabor Makeev <gabor.makejev@gmail.com>
2023-01-26 22:38:35 +0100

    feat: create User class

---
+++ User.php
@@ -0,0 +1,58 @@
+<?php
+
+class User
+{
+    public function __construct(
+        private int $id,
+        private string $firstName,
+        private string $lastName
+    ) {}
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+}
