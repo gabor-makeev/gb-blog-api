582871c296130a6bb97be5808945b0931cd6ae46
Gabor Makeev <gabor.makejev@gmail.com>
2023-02-06 22:30:31 +0100

    feat: add routing to http.php

--- http.php
+++ http.php
@@ -1,18 +1,49 @@
 <?php
 
+use Gabormakeev\GbBlogApi\Exceptions\AppException;
+use Gabormakeev\GbBlogApi\Http\Actions\Users\FindByUsername;
+use Gabormakeev\GbBlogApi\Http\ErrorResponse;
 use Gabormakeev\GbBlogApi\Http\Request;
-use Gabormakeev\GbBlogApi\Http\SuccessfulResponse;
+use Gabormakeev\GbBlogApi\Exceptions\HttpException;
+use Gabormakeev\GbBlogApi\Repositories\PostsRepository\SqlitePostsRepository;
+use Gabormakeev\GbBlogApi\Repositories\UsersRepository\SqliteUsersRepository;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $request = new Request($_GET, $_SERVER);
 
-$parameter = $request->query('some_parameter');
-$header = $request->header('Some-Header');
-$path = $request->path();
+try {
+    $path = $request->path();
+} catch (HttpException) {
+    (new ErrorResponse)->send();
+    return;
+}
 
-$response = new SuccessfulResponse([
-    'message' => 'Hello from PHP'
-]);
+$routes = [
+    '/users/show' => new FindByUsername(
+        new SqliteUsersRepository(
+            new PDO('sqlite:' . __DIR__ . '/blog.sqlite')
+        )
+    ),
+// TODO:
+//    '/posts/show' => new FindByUuid(
+//        new SqlitePostsRepository(
+//            new PDO('sqlite:' . __DIR__ . '/blog.sqlite')
+//        )
+//    )
+];
+
+if (!array_key_exists($path, $routes)) {
+    (new ErrorResponse('Not found'))->send();
+    return;
+}
+
+$action = $routes[$path];
+
+try {
+    $response = $action->handle($request);
+} catch (AppException $e) {
+    (new ErrorResponse($e->getMessage()))->send();
+}
 
 $response->send();
